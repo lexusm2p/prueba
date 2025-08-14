600a863136c545ae305d5474eaaee72894df9371
lexusm2p <lexusm2p@gmail.com>
2025-08-14 00:53:09 -0600

    Delete libs directory

--- libs/path.js
+++
@@ -1,6 +0,0 @@
-// /lib/path.js
-export const BASE = location.pathname.includes('/prueba/') ? '/prueba' : '';
-export const url = (p) => `${BASE}${p}`;
-
-import { url } from '../lib/path.js';
-const menu = await fetch(url('/data/menu.json')).then(r => r.json());
