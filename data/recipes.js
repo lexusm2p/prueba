84fff7c34a1b174b0da3ad0d69561400c9867d1f
lexusm2p <lexusm2p@gmail.com>
2025-08-14 08:39:50 -0600

    Create recipes.js

---
+++ data/recipes.js
@@ -0,0 +1,8 @@
+
+// data/recipes.js
+// Muy simplificado: cuánta unidad descuenta cada producto
+export const RECIPE = {
+  'Pan': 1, 'Carne 85g': 1, 'Queso amarillo': 1, 'Queso blanco': 1, 'Lechuga': 1, 'Jitomate': 1, 'Cebolla': 1,
+  'Mayonesa': 1, 'Catsup': 1, 'Mostaza': 1, 'Piña': 1, 'Tocino': 1, 'Jamón': 1, 'Salchicha': 1,
+  'Salsa Cheddar': 1, 'Aderezo chipotle': 1, 'Salsa Habanero': 1, 'Salsa Chimichurri': 1
+};
