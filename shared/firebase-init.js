2c0e83f0166362ae8d380dd11841efcabd6c053f
lexusm2p <lexusm2p@gmail.com>
2025-08-14 17:48:00 -0600

    Update and rename firebase.js to firebase-init.js

---
+++ shared/firebase-init.js
@@ -0,0 +1,10 @@
+// Rellena con tus credenciales reales de Firebase (App Web)
+export const firebaseConfig = {
+  apiKey: "REPLACE_ME",
+  authDomain: "REPLACE_ME.firebaseapp.com",
+  projectId: "REPLACE_ME",
+  storageBucket: "REPLACE_ME.appspot.com",
+  messagingSenderId: "REPLACE_ME",
+  appId: "1:REPLACE_ME:web:REPLACE_ME",
+  measurementId: "G-REPLACE"
+};
