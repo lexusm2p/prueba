06d840bbd8bbac0c48cc7b13962405ab762ff9c4
lexusm2p <lexusm2p@gmail.com>
2025-08-17 16:41:55 -0600

    Update firebase.js

--- shared/firebase.js
+++ shared/firebase.js
@@ -1,8 +1,20 @@
-export const firebaseConfig = window.FB_CONFIG || {
+
+// Firebase + Firestore
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
+import { getFirestore, collection, addDoc, onSnapshot, serverTimestamp, query, orderBy, doc, updateDoc, deleteDoc, getDoc, setDoc } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js";
+
+const firebaseConfig = {
   apiKey: "AIzaSyAidr-9HSNlfok5BOBer8Te8EflyV8VYi4",
   authDomain: "seven-de-burgers.firebaseapp.com",
   projectId: "seven-de-burgers",
   storageBucket: "seven-de-burgers.firebasestorage.app",
   messagingSenderId: "34089845279",
   appId: "1:34089845279:web:d13440c34e6bb7fa910b2a",
+  measurementId: "G-Q8YQJGL2XY"
 };
+
+export const app = initializeApp(firebaseConfig);
+export const db  = getFirestore(app);
+export { collection, addDoc, onSnapshot, serverTimestamp, query, orderBy, doc, updateDoc, deleteDoc, getDoc, setDoc };
+
+export { where, Timestamp, getDocs } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js";
