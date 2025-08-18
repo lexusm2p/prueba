48cf0e73349e1b230c1cafb91447fed1bed23a67
lexusm2p <lexusm2p@gmail.com>
2025-08-17 18:10:35 -0600

    Create firebase-config.js

---
+++ shared/firebase-config.js
@@ -0,0 +1,10 @@
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyAidr-9HSNlfok5BOBer8Te8EflyV8VYi4",
+  authDomain: "seven-de-burgers.firebaseapp.com",
+  projectId: "seven-de-burgers",
+  storageBucket: "seven-de-burgers.firebasestorage.app",
+  messagingSenderId: "34089845279",
+  appId: "1:34089845279:web:d13440c34e6bb7fa910b2a",
+  measurementId: "G-Q8YQJGL2XY"
+};
