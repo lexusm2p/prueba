b8380aa4cdc95c4d6da65bf70c505f7df7e4cce8
lexusm2p <lexusm2p@gmail.com>
2025-08-18 18:01:34 -0600

    Create products.js

---
+++ shared/products.js
@@ -0,0 +1,30 @@
+export const MENU = {
+  minis: [
+    { id:'starter-mini', name:'Starter Mini', base:'mini',
+      ingredients:['Pan','Carne 85g','Queso amarillo','Queso blanco','Lechuga','Jitomate','Cebolla','Mayonesa','Catsup','Mostaza'],
+      sugSauce:'Mostaza dulce'
+    },
+    { id:'koopa-mini', name:'Koopa Mini', base:'mini',
+      ingredients:['Pan','Carne 85g','Queso blanco','Piña','Tocino','Lechuga','Jitomate','Cebolla','Mayonesa','Catsup','Mostaza'],
+      sugSauce:'Chimichurri'
+    },
+    { id:'fatality-mini', name:'Fatality Mini', base:'mini',
+      ingredients:['Pan','Carne 85g','Salsa Cheddar','Tocino','Salsa Habanero','Lechuga','Jitomate','Cebolla','Mayonesa','Catsup','Mostaza'],
+      sugSauce:'Habanero'
+    },
+  ],
+  grandes: [
+    { id:'starter', name:'Starter Burger', base:'grande',
+      ingredients:['Pan','Carne 85g','Queso amarillo','Queso blanco','Lechuga','Jitomate','Cebolla','Mayonesa','Catsup','Mostaza'],
+      sugSauce:'Mostaza dulce'
+    },
+    { id:'koopa', name:'Koopa Crunch', base:'grande',
+      ingredients:['Pan','Carne 85g','Queso blanco','Piña','Tocino','Lechuga','Jitomate','Cebolla','Mayonesa','Catsup','Mostaza'],
+      sugSauce:'Chimichurri'
+    },
+    { id:'fatality', name:'Fatality Flame', base:'grande',
+      ingredients:['Pan','Carne 85g','Salsa Cheddar','Tocino','Salsa Habanero','Lechuga','Jitomate','Cebolla','Mayonesa','Catsup','Mostaza'],
+      sugSauce:'Habanero'
+    },
+  ]
+};
